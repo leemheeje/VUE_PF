c744e3abbaca4e649b23bbc0637078800e53bae3
hjlim@makeulike.com <hjlim@makeulike.com>
2018-09-06 11:55:06 +0900

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/VUE_PF/dist/',
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
